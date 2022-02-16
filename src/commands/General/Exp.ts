c7e10e72ad2344c470e9cc4b082cc6e7693e47bd
Killercrosx <96986315+Killercrosx@users.noreply.github.com>
2022-02-16 16:23:16 +0300

    Update Exp.ts

--- src/commands/General/Exp.ts
+++ src/commands/General/Exp.ts
@@ -24,6 +24,6 @@ export default class Command extends BaseCommand {
             // username = contact.notify || contact.vname || contact.name || user.split('@')[0]
             username = user.split('@')[0]
         }
-        return void (await M.reply(`*${username} Exp: ${(await this.client.getUser(user)).Xp || 0}*`))
+        return void (await M.reply(``🧧 *${username}'s* \n\n🚀 *XP: ${data.Xp || 0}*`}`))
     }
 }
