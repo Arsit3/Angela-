481f7b42b8715e966ea05d662d31ae21ceabd59f
Killercrosx <96986315+Killercrosx@users.noreply.github.com>
2022-02-16 15:42:14 +0300

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -80,8 +80,8 @@ export default class Command extends BaseCommand {
 					})();
 					return {
 						categories,
-						pack: pack[1] || "🌟 Here you go ",
-						author: pack[2] || "Chitoge 🌟",
+						pack: pack[1] || "Infinity♾️ ",
+						author: pack[2] || "Angela🚀",
 						quality,
 						type: StickerTypes[
 							parsedArgs.flags.includes("--crop") ||
